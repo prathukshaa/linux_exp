a5cb4e3b4ea4a915692a8c6245d330d3f756a20e
prathukshaa <prathukshaa2210055@ssn.edu.in>
2025-07-29 10:20:30 +0000

    Add DVC tracking, tests, pylint, mypy, and wheel build files

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Hello, Poetry and Conda!")
+def add(a: int, b: int) -> int:
+    return a + b
+
+print(add(2, 3))
--- test_main.py
+++ test_main.py
@@ -10,3 +10,12 @@ def test_raises_error():
     with pytest.raises(ValueError):
         raises_error()
 
+import pytest
+from main import add
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_add_error():
+    with pytest.raises(TypeError):
+        add("a", 1)
